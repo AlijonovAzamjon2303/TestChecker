81ce88fb7c42ee89f6d5e4b60192af564fdbbd2f
Azamjon <alijonovrobo@gmail.com>
2025-01-12 02:21:40 +0500

    chanegd

--- Services.py
+++ Services.py
@@ -6,8 +6,10 @@ def add_test(keys):
     last_id = 1
     if os.path.exists(path):
         with open(path, "r") as file:
-            ls = file.read().split()
-            last_id = max(last_id, int(ls[0]))
+            ls = file.readlines()
+            for line in ls:
+                last_id = max(last_id, int(line.split()[0]))
+
 
     with open(path, 'a') as file:
         file.write(f"{last_id + 1} {keys}")
