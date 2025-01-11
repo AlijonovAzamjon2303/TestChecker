536e091566e9c83a7e4929b40125d546e69732ce
Azamjon <alijonovrobo@gmail.com>
2025-01-12 00:44:17 +0500

    bot start commanda uchun ishladi

---
+++ main.py
@@ -0,0 +1,24 @@
+import asyncio
+import logging
+
+from aiogram import Bot, Dispatcher, types, F
+from aiogram.filters import Command
+from aiogram.types import Message
+
+from config import BOT_TOKEN
+
+bot = Bot(BOT_TOKEN)
+dp = Dispatcher()
+
+@dp.message(Command("start"))
+async def start_cmd(message: Message):
+    user_id = message.from_user.id
+    user_first_name = message.from_user.first_name
+    await message.answer(f"Assalomu alaykum {user_first_name} sizning id : {user_id}")
+
+async def main():
+    await dp.start_polling(bot)
+
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.INFO)
+    asyncio.run(main())
