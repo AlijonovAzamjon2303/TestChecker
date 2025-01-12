48fbe9b59780f97293daee15716f57d7b09476d6
Azamjon <alijonovrobo@gmail.com>
2025-01-12 11:28:29 +0500

    a

--- Services.py
+++ Services.py
@@ -1,6 +1,7 @@
 import os
 
 path = "tests.txt"
+path_act = "answers.txt"
 
 async def add_test(keys):
     last_id = 1
@@ -29,3 +30,32 @@ async def clear():
         os.remove(path)
     else:
         return 0
+
+async def add_ans(chat_id, test_id, first_name, answer):
+    tests = []
+    with open(path, "r") as file:
+        tests = file.readlines()
+
+    answer = answer.split()
+
+    cnt = 0
+    for test in tests:
+        test = test.split()
+        if test[0] == answer[0]:
+           for i in range(min(len(test[0]), answer[0])):
+               if test[1][i] == answer[1][i]:
+                   cnt += 1
+
+    with open(path_act, "a") as file:
+        file.write(f"{chat_id} {test_id} {first_name} {answer} {cnt}")
+
+    return cnt
+
+async def show_all_act():
+    acts = ""
+    with open(path_act, "r") as file:
+        for line in file.readlines():
+            line = line.strip()
+            acts += line + "\n"
+
+    return acts
--- main.py
+++ main.py
@@ -13,7 +13,7 @@ BOT_TOKEN = os.getenv('BOT_TOKEN')
 bot = Bot(BOT_TOKEN)
 dp = Dispatcher()
 
-ADMIN = 6335120359
+ADMINS = [6335120359]
 
 @dp.message(Command("start"))
 async def start_cmd(message: Message):
@@ -21,16 +21,25 @@ async def start_cmd(message: Message):
     user_first_name = message.from_user.first_name
     await message.answer(f"Assalomu alaykum {user_first_name} sizning id: {user_id}")
     await message.answer("Quyidagi buyruqlardan birini tanlang\n/add_test\n/check_test")
+    if user_id in ADMINS:
+        await message.answer("\n/show_all_test\n")
 
 @dp.message(Command("clear"))
 async def clear(message: Message):
-    ans = await Services.clear()
-    if ans == 0:
-        await message.answer("Baza tozalandi")
+    if message.from_user.id == 6335120359:
+        ans = await Services.clear()
+        if ans == 0:
+            await message.answer("A'zamjon baza tozalandi")
+
+@dp.message(Command("show_all_act"))
+async def show_acts(message: Message):
+    acts = await Services.show_all_act()
+
+    await message.answer(acts)
 
 @dp.message(Command("add_test"))
 async def add_test_cmd(message: Message):
-    if message.from_user.id == ADMIN:
+    if message.from_user.id in ADMINS:
         await message.answer("1a2b3c4d ... kabi test javoblarini kiriting")
     else:
         await message.answer("@AzamjonAlijonov bilan bog'laning")
@@ -40,7 +49,7 @@ async def show_all(message: Message):
     tests = await Services.show_all_test()
     await message.answer(f"{tests}")
 
-@dp.message(lambda message: message.from_user.id == ADMIN)
+@dp.message(lambda message: message.from_user.id in ADMINS)
 async def save_test(message: Message):
     test_keys = message.text.strip()  # Test javoblarini olish
     print(f"Admin test javoblari: {test_keys}")  # Admin tomonidan yuborilgan test javoblarini konsolga chiqarish
@@ -52,6 +61,16 @@ async def save_test(message: Message):
     except Exception as e:
         print(f"Xatolik: {e}")  # Xatolik yuz bersa, uni konsolga chiqarish
         await message.answer("Testni saqlashda xatolik yuz berdi!")
+
+@dp.message()
+async def check_ans(message: Message):
+    ans = message.text.split()
+    chat_id = message.from_user.id
+    test_id = ans[0]
+    first_name = message.from_user.first_name
+    cnt = await Services.add_ans(message.from_user.id, test_id, first_name, ans[1])
+
+    await message.answer(f"Humatli {first_name} siz {test_id} id li tesdan {cnt} ta topdingiz")
 
 async def main():
     await dp.start_polling(bot)
